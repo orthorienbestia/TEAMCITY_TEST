e19b914adc0b8db7dc163040047cdc33d3708a61
Akshay Khandizod <orthorienbestia@github.com>
2021-09-24 18:50:23 +0530

    editor script added

---
+++ Assets/Editor/MyEditorScript.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEditor;
+
+public class MyEditorScript
+{
+    static void PerformBuild ()
+    {
+        try
+        {
+            string[] scenes = { "Assets/Scenes/MainScene.unity" };
+            BuildPipeline.BuildPlayer(scenes,"/home/akshay/Unity Projects/Cloud Build Test/Builds/androidbuild.apk",BuildTarget.Android,BuildOptions.None);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+        
+    }
+}
